768a483264a10dd8639d585d4a46986c9effb769
Aydar <sekret39-01@inbox.ru>
2022-09-28 18:49:58 +0300

    neww

--- components/PopupWithConfirmation.js
+++ components/PopupWithConfirmation.js
@@ -4,7 +4,7 @@ export default class PopupWithConfirmation extends Popup {
   constructor(popupSelector, popupConfiguration) {
     super(popupSelector, popupConfiguration);
     this._btn = this._popup.querySelector(".form__save_delete");
-    this._popupButtonTextContent = this._btnPopup.textContent;
+    // this._popupButtonTextContent = this._btnPopup.textContent;
     this.onSubmit = () => {};
   }
 
